a2d64c08b8031b40383cabd47509499423a0e0e1
Dhruv Shah <dhruvshah2000@hotmail.com>
2021-01-09 19:38:14 -0600

    Updates

---
+++ precache-manifest.239bc8f222e05bacf2107f288f7e3b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d336f060be8335b07cfaff302b8951",
+    "url": "/Facebook-Comment-Clone/index.html"
+  },
+  {
+    "revision": "0acf32e6d7bf89fa5d9a",
+    "url": "/Facebook-Comment-Clone/static/css/2.1a08bad1.chunk.css"
+  },
+  {
+    "revision": "4e48a90303632b124f43",
+    "url": "/Facebook-Comment-Clone/static/css/main.46794b47.chunk.css"
+  },
+  {
+    "revision": "0acf32e6d7bf89fa5d9a",
+    "url": "/Facebook-Comment-Clone/static/js/2.42eb14a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Facebook-Comment-Clone/static/js/2.42eb14a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e48a90303632b124f43",
+    "url": "/Facebook-Comment-Clone/static/js/main.5cb0a5eb.chunk.js"
+  },
+  {
+    "revision": "c77c78d2c9949d953258",
+    "url": "/Facebook-Comment-Clone/static/js/runtime-main.ce4453ed.js"
+  }
+]);
